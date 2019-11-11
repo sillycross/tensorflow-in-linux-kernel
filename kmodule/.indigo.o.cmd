8d09547730b51fdf20499d8d8814c7cecaa7b0f2
Haoran Xu <haoranxu510@gmail.com>
2019-11-11 13:15:46 -0800

    rm tron files

--- kmodule/.indigo.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/lemon/indigo-in-kernel/kmodule/indigo.o := ld -m elf_x86_64 -z max-page-size=0x200000   -r -o /home/lemon/indigo-in-kernel/kmodule/indigo.o /home/lemon/indigo-in-kernel/kmodule/indigo_utils.o /home/lemon/indigo-in-kernel/kmodule/main.o /home/lemon/indigo-in-kernel/kmodule/math.o /home/lemon/indigo-in-kernel/kmodule/libgraph.pic.a 
--- kmodule/.tmp_versions/indigo.mod
+++
@@ -1,3 +0,0 @@
-/home/lemon/indigo-in-kernel/kmodule/indigo.ko
-/home/lemon/indigo-in-kernel/kmodule/indigo_utils.o /home/lemon/indigo-in-kernel/kmodule/main.o /home/lemon/indigo-in-kernel/kmodule/math.o /home/lemon/indigo-in-kernel/kmodule/libgraph.pic.a
-
